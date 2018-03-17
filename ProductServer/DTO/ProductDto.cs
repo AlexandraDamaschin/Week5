51ea61ff56f656c5377ed88a466f67af9186e661
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-17 21:26:04 +0000

    dto created

---
+++ ProductServer/DTO/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductServer.DTO
+{
+    public class ProductDto
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int ReOrderLevel { get; set; }
+        public float Price { get; set; }
+        public int SupplierID { get; set; }
+
+    }
+}
---
+++ ProductServer/DTO/SupplierDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductServer.DTO
+{
+    public class SupplierDto
+    {
+        public int SupplierID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
