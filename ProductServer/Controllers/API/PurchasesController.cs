ce41bde2e4bc18f9c9984838f36357620aefadd0
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-17 21:38:51 +0000

    basic purchases controller web api created

--- ProductServer/Controllers/API/PurchasesController.cs
+++ ProductServer/Controllers/API/PurchasesController.cs
@@ -1,12 +1,9 @@
 ﻿using ProductServer.DAL;
-using ProductServer.DTO;
 using ProductServer.Models.ProductModels;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
+using CommonObjects;
 using System.Web.Http;
 
 namespace ProductServer.Controllers.API
@@ -50,79 +47,6 @@ namespace ProductServer.Controllers.API
         public async Task<IList<Product>> GetReorderList()
         {
             return await (context as IProductRepository).GetReorderList();
-        }
-
-        // Task 2
-        [HttpPost]
-        [Route("add/SupplierProduct")]
-        public async Task<Product> postProductSupplier(ProductDto newProduct)
-        {
-            // Here we get a Product DTO and create a new product based on the DTO 
-            // with associated supplier 
-            // We call the Post Entity method to add and update the db context
-            // We return the resulting inserted object which ha all the fields filled in 
-            // as a result of the Insertion
-            Product inserted = await (context as IProductRepository).PostEntity(
-                new Product
-                {
-                    Description = newProduct.Description,
-                    assocSupplier = new Supplier
-                    {
-                        Address = newProduct.Supplier.Address,
-                        Name = newProduct.Supplier.Name
-                    },
-                    Price = newProduct.Price,
-                    Quantity = newProduct.Quantity,
-                    ReOrderLevel = newProduct.ReOrderLevel
-                });
-            return inserted;
-        }
-        // Task 3 But you would have to change the model as deletes are cascading at the moment
-        [HttpDelete]
-        [Route("delete/Supplier/{sid:int}")]
-        public async Task<Supplier> DeleteSupplier(int sid)
-        {
-            return await (context as ISupplierRepository).delete(sid);
-        }
-
-        [HttpGet]
-        [Route("get/SupplierWithProducts/Sname/{name}")]
-        public SupplierDto GetSupplierWithProducts(string name)
-        {
-            // This query is long winded because the navigation propoerty only goes from Product  to Supplier.
-            // Two way navigation or more natural navigation from Supplier to Product Master to Detail would yield 
-            // more compact queries.
-            // Could and should move all this logic in the Iproduct repository interface  
-            Supplier supplier = (context as ISupplierRepository).getEntities().Result // Result of async task is a supplier list
-                                 .FirstOrDefault(s => s.Name == name); // Should only be one
-            // if there is a supplier then get all the products that this supplier supplies
-            if (supplier != null)
-            {
-                // Construct Supplier DTO. Auto Mapper would be good here.
-                SupplierDto sDTO = new SupplierDto
-                {
-                    ID = supplier.ID,
-                    Name = supplier.Name,
-                    Address = supplier.Address,
-                    Products = new List<ProductDto>()
-                };
-                // 
-                var products = (context as IProductRepository).getEntities().Result.ToList().Where(p => p.SupplierID == supplier.SupplierID);
-                foreach (var p in products)
-                {
-                    sDTO.Products.Add(new ProductDto
-                    {
-                        ProductID = p.ProductID,
-                        Description = p.Description,
-                        SupplierID = p.SupplierID,
-                        Price = p.Price,
-                        Quantity = p.Quantity,
-                        ReOrderLevel = p.ReOrderLevel
-                    });
-                }
-                return sDTO;
-            }
-            return null;
         }
     }
 }
