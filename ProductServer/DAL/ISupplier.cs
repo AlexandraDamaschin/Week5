eb619069999d25be7bacc6f944a0d3b6da1d136c
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-17 20:48:45 +0000

    interfaces created

---
+++ ProductServer/DAL/ISupplier.cs
@@ -0,0 +1,15 @@
+﻿using ProductServer.Models.ProductModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductServer.DAL
+{
+    interface ISupplier : IDisposable
+    {
+        List<Supplier> GetSuppliers();
+        Supplier GetSupplierByID(int id);
+    }
+}
