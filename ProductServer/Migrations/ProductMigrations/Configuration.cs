2f07fe8c5789f2a2c95b8de3a4007935a7a63b36
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-18 11:09:31 +0000

    seed Products based on a random supplierId and a random quantity

--- ProductServer/Migrations/ProductMigrations/Configuration.cs
+++ ProductServer/Migrations/ProductMigrations/Configuration.cs
@@ -17,9 +17,7 @@ namespace ProductServer.Migrations.ProductMigrations
         protected override void Seed(ProductServer.Models.ProductModels.ProductDbContext context)
         {
             SeedSupliers(context);
-
-
-
+            SeedProducts(context);
         }
 
         //seed supliers
@@ -32,6 +30,29 @@ namespace ProductServer.Migrations.ProductMigrations
                     new Supplier { Name="Plumber Man", Address="Unit 12 Ballymun"  }
               });
             context.SaveChanges();
+        }
+
+        //seed Products
+        public void SeedProducts(ProductDbContext context)
+        {
+            Random r = new Random();
+            context.Products.AddOrUpdate(s => s.Description,
+                new Product[] {
+                    new Product{  Description="9 inch Nails", SupplierID = GetRandomSupplierID(context), Price = 0.20f, Quantity = r.Next(100,200) },
+                    new Product{  Description="Size 9 shoe", SupplierID = GetRandomSupplierID(context), Price = 22.10f, Quantity = r.Next(10,15) },
+                    new Product{  Description="Large Overalls", SupplierID = GetRandomSupplierID(context), Price = 42.10f, Quantity = r.Next(10,15) },
+                    new Product{  Description="Draining Boards", SupplierID = GetRandomSupplierID(context), Price = 142.10f, Quantity = r.Next(5,7) },
+                });
+            context.SaveChanges();
+        }
+
+        // get a random supplied Id
+        private int GetRandomSupplierID(ProductDbContext Context)
+        {
+            return Context.Suppliers.Select(s => new { s.SupplierID, order = Guid.NewGuid() })
+                                    .OrderBy(o => o.order)
+                                    .Select(s => s.SupplierID)
+                                    .First();
         }
     }
 }
